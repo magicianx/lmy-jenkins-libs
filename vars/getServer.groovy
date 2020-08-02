37e1d5d97027fa4d24f0933c99be8fc2bd955678
吴鑫 <16269714@qq.com>
2020-08-02 15:33:56 +0800

    增加获取远程主机函数

---
+++ vars/getServer.groovy
@@ -0,0 +1,15 @@
+// vars/getServer.groovy
+def call(serverName) {
+    def remote = [:]
+    remote.name = 'gnode2'
+    remote.host = 'api1.vchat.club'
+    remote.allowAnyHosts = true
+    withCredentials([sshUserPrivateKey(
+            keyFileVariable:"key",
+            credentialsId:"gnode-key",
+            usernameVariable:"userName")]) {
+        remote.user = userName
+        remote.identityFile = key
+    }
+    println("serverName")
+}
